f87fc22dd1af50efe1d98a24866103234a03f82c
YanWQ-monad <643172561@qq.com>
2016-08-05 08:17:13 +0800

    Create Merge Sort.cpp

---
+++ Cpp/Algorithm/Sort/Merge Sort.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+
+template<typename T>
+void mergesort(T *begin,T *end,T *Temp) {
+	if (begin>=end) return;
+	int size=end-begin,mid=size>>1;
+	mergesort(begin,&begin[mid],Temp);
+	mergesort(&begin[mid+1],end,&Temp[mid+1]);
+	int l=0,r=mid+1,now=0;
+	while (l<=mid && r<=size) {
+		Temp[now++]=begin[l]<begin[r]? begin[l++]:begin[r++];
+	}
+	while (l<=mid)  Temp[now++]=begin[l++];
+	while (r<=size) Temp[now++]=begin[r++];
+	for (int i=0;i<=size;i++)
+		begin[i]=Temp[i];
+}
+
+int n,a[100],temp[100];
+int main() {
+	scanf("%d",&n);
+	for (int i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	mergesort(a,a+n-1,temp);
+	for (int i=0;i<n;i++)
+		printf("%d ",a[i]);
+	return 0;
+}
