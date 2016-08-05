f7dbb19b4cd742d0271b3434f37a41443d3edeff
YanWQ-monad <643172561@qq.com>
2016-08-05 10:56:58 +0800

    Create 1242.cpp

---
+++ Cpp/Exam-answer/smoj.nhedu.net/1242.cpp
@@ -0,0 +1,29 @@
+// 1242.cpp
+#include <cstdio>
+
+const int MAXN = 10;
+int n,s[MAXN],link[MAXN];
+void print() {
+	for (int i=1;i<=n && s[i]!=0;i++)
+		printf("%d ",s[i]);
+	printf("\n");
+}
+void dfs(int x) {
+	for (int il=0,i=link[0];i!=0;il=i,i=link[i]) {
+		link[il]=link[i];
+		s[x]=i;
+		if (x<n) dfs(x+1);
+		else print();
+		link[il]=i;
+		s[x]=0;
+	}
+}
+int main() {
+	freopen("1242.in","r",stdin);
+	freopen("1242.out","w",stdout);
+	scanf("%d",&n);
+	for (int i=0;i<n;i++)
+		link[i]=i+1;
+	dfs(1);
+	return 0;
+}
