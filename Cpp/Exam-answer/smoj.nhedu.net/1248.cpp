1f274630ebcc140531904652bcd53cd8dd410470
YanWQ-monad <643172561@qq.com>
2016-08-05 19:30:11 +0800

    Update 1248.cpp

--- Cpp/Exam-answer/smoj.nhedu.net/1248.cpp
+++ Cpp/Exam-answer/smoj.nhedu.net/1248.cpp
@@ -6,6 +6,15 @@ int sum=0,n,s[MAXN];
 long long k;
 bool used[MAXN];
 
+void read() {
+	#if !defined(SAG_COM) && (defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__))
+	scanf("%d %I64d",&n,&k);
+	#else
+	scanf("%d %lld",&n,&k);
+	#endif
+	k--;
+	used[0]=true;
+}
 void print() {
 	for (int i=0;i<n;i++)
 		printf("%d ",s[i]);
@@ -17,31 +26,22 @@ long long factorial(int n) {
 		sum*=i;
 	return sum;
 }
-void dfs(int x) {
-	if (x==1) {
-		for (int i=0;used[i];s[n-x]=++i);
-		print();
-		return;
+void work() {
+	for (int x=n;x>1;x--) {
+		int mod=(k/factorial(x-1))%x,i;
+		for (i=1;used[i];i++);
+		for (int j=0;j<mod;j+=!used[i],i++);
+		for (;used[i];i++);
+		used[s[n-x]=i]=true;
 	}
-	int mod=(k/factorial(x-1))%x,i;
-	//printf("mod=(%d/%d)%%%d=%d\n",k,countsum(x-1),x,mod);
-	for (i=1;used[i];i++);
-	for (int j=0;j<mod;j+=!used[i],i++);
-	for (;used[i];i++);
-	used[s[n-x]=i]=true;
-	dfs(x-1);
+	for (int i=0;used[i];s[n-1]=++i);
+	print();
 }
 
 int main() {
 	freopen("1248.in","r",stdin);
 	freopen("1248.out","w",stdout);
-	#if !defined(SAG_COM) && (defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__))
-	scanf("%d %I64d",&n,&k);
-	#else
-	scanf("%d %lld",&n,&k);
-	#endif
-	k--;
-	used[0]=true;
-	dfs(n);
+	read();
+	work();
 	return 0;
 }
