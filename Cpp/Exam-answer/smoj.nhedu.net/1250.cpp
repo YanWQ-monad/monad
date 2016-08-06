e88c9076a260365cff452172c690440748786da2
YanWQ-monad <643172561@qq.com>
2016-08-06 21:26:42 +0800

    Update 1250.cpp

--- Cpp/Exam-answer/smoj.nhedu.net/1250.cpp
+++ Cpp/Exam-answer/smoj.nhedu.net/1250.cpp
@@ -1,29 +1,17 @@
 // 1250.cpp
 #include <cstdio>
-#include <string>
-#include <map>
-using std::string;
-using std::map;
-map<string,int>p;
 int n;
-char temp[25];
-string stt;
+char temp[25],mxor[25];
 int main() {
 	freopen("1250.in","r",stdin);
 	freopen("1250.out","w",stdout);
 	scanf("%d",&n);
-	for (int i=0;i<n;i++) {
+	for (int i=0;i<n*2-1;i++) {
 		scanf("%s",temp);
-		stt=temp;
-		p[stt]++;
+		for (int j=0;temp[j]!='\0';j++)
+			mxor[j]^=temp[j];
 	}
-	for (int i=0;i<n-1;i++) {
-		scanf("%s",temp);
-		stt=temp;
-		if ((--p[stt])==0)
-			p.erase(stt);
-	}
-	for (map<string,int>::iterator iter=p.begin();iter!=p.end();iter++)
-		printf("%s",iter->first.c_str());
+	for (int i=0;mxor[i]!='\0';i++)
+		printf("%c",mxor[i]);
 	return 0;
 }
