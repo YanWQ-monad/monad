16e3e55359d30391a21159b5efdeb16c38d8eadc
YanWQ-monad <643172561@qq.com>
2016-08-06 15:46:16 +0800

    Update 1251.cpp

--- Cpp/Exam-answer/smoj.nhedu.net/1251.cpp
+++ Cpp/Exam-answer/smoj.nhedu.net/1251.cpp
@@ -1,28 +1,15 @@
 // 1251.cpp
-#include <cstring>
 #include <cstdio>
 
-char temp[64];
-short keyboard[128];
 int finger[9];
-void init(const char *keys,int finger) {
-	for (int i=0;keys[i]!='\0';i++)
-		keyboard[(int)keys[i]]=finger;
-}
+short key[]={8,0,0,0,0,6,8,7,8,8,1,2,3,4,4,5,5,6,7,0,8,0,8,0,0,0,1,4,3,3,3,4,4,5,6,5,6,7,5,5,7,8,1,4,2,4,5,4,2,2,5,1,8,0,8};
+#define keys(x) key[x-'\'']
 int main() {
 	freopen("1251.in","r",stdin);
 	freopen("1251.out","w",stdout);
-	init(" 1QAZ",1);
-	init("2WSX",2);
-	init("3EDC",3);
-	init("4RFV5TGB",4);
-	init("6YHN7UJM",5);
-	init("8IK,",6);
-	init("9OL.",7);
-	init("0P;/\'[]-=",8);
 	char temp;
 	while ((temp=getchar())!=-1)
-		finger[keyboard[(int)temp]]++;
+		finger[keys(temp)]++;
 	for (int i=1;i<=8;i++)
 		printf("%d\n",finger[i]);
 	return 0;
