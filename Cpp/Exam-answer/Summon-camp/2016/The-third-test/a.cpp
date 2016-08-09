db203b4e0966b61e841947e70731d42971463647
YanWQ-monad <643172561@qq.com>
2016-08-09 18:42:07 +0800

    Create a.cpp

---
+++ Cpp/Exam-answer/Summon-camp/2016/The-third-test/a.cpp
@@ -0,0 +1,24 @@
+// a.cpp
+#include <cstdio>
+#define min(x,y) ((x)<(y)? (x):(y))
+
+const int MAXN = 110,MAXM = 50010;
+int n,a[MAXN],m,f[MAXM];
+void dp() {
+	for (int i=1;i<=n;i++)
+		for (int k=a[i];k<=m;k++)
+			f[k]=min(f[k],f[k-a[i]]+1);
+}
+int main() {
+	freopen("a.in","r",stdin);
+	freopen("a.out","w",stdout);
+	scanf("%d",&n);
+	for (int i=1;i<=n;i++)
+		scanf("%d",&a[i]);
+	scanf("%d",&m);
+	for (int i=1;i<=m;i++) f[i]=0x3fffffff;
+	f[0]=0;
+	dp();
+	printf("%d\n",f[m]==0x3fffffff? -1:f[m]);
+	return 0;
+}
