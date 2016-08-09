1d0a3c41ff450df116bc2ff338706de7e0651dd7
YanWQ-monad <643172561@qq.com>
2016-08-09 14:29:25 +0800

    Create b.cpp

---
+++ Cpp/Exam-answer/Summon-camp/2016/The-third-test/b.cpp
@@ -0,0 +1,33 @@
+// b.cpp
+#include <algorithm>
+#include <cstdio>
+using std::sort;
+#define max(x,y) ((x)>(y)? (x):(y))
+
+const int MAXN = 10010;
+int n,f[MAXN],max;
+struct NODE{int x,y;}q[MAXN];
+int NodeSort(NODE lhs,NODE rhs) {
+	return lhs.y<rhs.y;
+}
+void dp() {
+	for (int i=1;i<=n;i++) {
+		f[i]=1;
+		for (int j=1;j<i;j++) {
+			if (q[j].y>q[i].x) continue;
+			f[i]=max(f[i],f[j]+1);
+		}
+		max=f[i]>max? f[i]:max;
+	}
+}
+int main() {
+	freopen("b.in","r",stdin);
+	freopen("b.out","w",stdout);
+	scanf("%d",&n);
+	for (int i=1;i<=n;i++)
+		scanf("%d %d",&q[i].x,&q[i].y);
+	sort(q+1,q+n+1,NodeSort);
+	dp();
+	printf("%d\n",max);
+	return 0;
+}
