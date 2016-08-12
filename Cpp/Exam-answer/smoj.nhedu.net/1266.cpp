9d3f5fc6cabc18931e30c4855fd008f0d8fb6e33
YanWQ-monad <yanwqmonad@gmail.com>
2016-08-13 06:35:50 +0800

    Create 1266.cpp

---
+++ Cpp/Exam-answer/smoj.nhedu.net/1266.cpp
@@ -0,0 +1,23 @@
+// 1266.cpp
+#include <cstdio>
+const int MAXN = 50;
+int fibonacci[MAXN+1];
+void InitFibonacci() {
+	fibonacci[0]=0;
+	fibonacci[1]=1;
+	for (int i=2;i<=MAXN;i++)
+		fibonacci[i]=fibonacci[i-1]+fibonacci[i-2];
+}
+int main() {
+	InitFibonacci();
+	freopen("1266.in","r",stdin);
+	freopen("1266.out","w",stdout);
+	int r;
+	scanf("%d",&r);
+	for (int rt=0,n;rt<r;rt++) {
+		scanf("%d",&n);
+		if (n==0) puts("1 0");
+		else printf("%d %d\n",fibonacci[n-1],fibonacci[n]);
+	}
+	return 0;
+}
