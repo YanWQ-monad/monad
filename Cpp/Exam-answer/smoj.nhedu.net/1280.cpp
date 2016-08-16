4bd619d14c1be318fb7aeb2e4ad85a83eee40e85
YanWQ-monad <yanwqmonad@gmail.com>
2016-08-16 08:09:57 +0800

    Create 1280.cpp

---
+++ Cpp/Exam-answer/smoj.nhedu.net/1280.cpp
@@ -0,0 +1,25 @@
+// 1280.cpp
+#include <algorithm>
+#include <cstdio>
+using std::max;
+
+int n,m,a[1024],ans=0;
+int main() {
+	freopen("1280.in","r",stdin);
+	freopen("1280.out","w",stdout);
+	scanf("%d %d",&n,&m);
+	for (int i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	for (int i=0,hungry,sum;i<n;i++) {
+		hungry=m;
+		sum=0;
+		for (int j=i;j<n && hungry;j++)
+			if (a[j]<=hungry) {
+				hungry -= a[j];
+				sum++;
+			}
+		ans = max(ans,sum);
+	}
+	printf("%d\n",ans);
+	return 0;
+}
