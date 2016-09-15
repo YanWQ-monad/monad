68d0af52324323e542904685d6d05e446a4f7f7e
YanWQ-monad <yanwqmonad@gmail.com>
2016-09-16 03:24:53 +0800

    Create 1061.cpp

---
+++ Cpp/Exam-answer/smoj.nhedu.net/1061.cpp
@@ -0,0 +1,11 @@
+// 1061.cpp
+#include <cstdio>
+
+int a,b,c;
+int main() {
+	freopen("1061.in","r",stdin);
+	freopen("1061.out","w",stdout);
+	scanf("%d %d %d",&a,&b,&c);
+	printf("%d",a-(b+c));
+	return 0;
+}
