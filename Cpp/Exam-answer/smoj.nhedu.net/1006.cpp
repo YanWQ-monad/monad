f9eb24e51632dc42f8ebc66e1d023a4893cef5f9
YanWQ-monad <yanwqmonad@gmail.com>
2016-09-16 02:27:45 +0800

    Create 1006.cpp

---
+++ Cpp/Exam-answer/smoj.nhedu.net/1006.cpp
@@ -0,0 +1,11 @@
+// 1006.cpp
+#include <cstdio>
+
+int a,b;
+int main() {
+	freopen("1006.in","r",stdin);
+	freopen("1006.out","w",stdout);
+	scanf("%d %d",&a,&b);
+	printf("%d",a+b);
+	return 0;
+}
