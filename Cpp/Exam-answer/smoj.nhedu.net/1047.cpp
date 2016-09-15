a6a4b973708c0fa219b5ac779dd332f6b5888e8f
YanWQ-monad <yanwqmonad@gmail.com>
2016-09-16 02:58:56 +0800

    Create 1047.cpp

---
+++ Cpp/Exam-answer/smoj.nhedu.net/1047.cpp
@@ -0,0 +1,11 @@
+// 1047.cpp
+#include <cstdio>
+using namespace std;
+int n;
+int main() {
+	freopen("1047.in","r",stdin);
+	freopen("1047.out","w",stdout);
+	scanf("%d",&n);
+	printf("%d",(n+1)*n/2);
+	return 0;
+}
