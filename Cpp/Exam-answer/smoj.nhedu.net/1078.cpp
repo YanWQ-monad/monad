60dbaded3643f10c0baaffe94bb515db289e8027
YanWQ-monad <yanwqmonad@gmail.com>
2016-09-16 03:55:03 +0800

    Create 1078.cpp

---
+++ Cpp/Exam-answer/smoj.nhedu.net/1078.cpp
@@ -0,0 +1,10 @@
+// 1078.cpp
+#include <cstdio>
+int main() {
+	freopen("1078.in","r",stdin);
+	freopen("1078.out","w",stdout);
+	for (int i=105;i<=999;i+=7)
+		printf("%d ",i);
+	printf("\n");
+	return 0;
+}
