d6542278f017c3c12ad2fc4729a538a863e1a3e0
YanWQ-monad <yanwqmonad@gmail.com>
2016-09-16 03:37:42 +0800

    Create 1135.cpp

---
+++ Cpp/Exam-answer/smoj.nhedu.net/1135.cpp
@@ -0,0 +1,26 @@
+// 1135.cpp
+#include <algorithm>
+#include <cstdio>
+using std::max;
+
+int n,d[50],m;
+bool f[1002][1002];
+int main() {
+	freopen("1135.in","r",stdin);
+	freopen("1135.out","w",stdout);
+	scanf("%d",&n);
+	for (int i=1,temp;i<=n;i++) {
+		scanf("%d",&temp);
+		d[i]=temp;
+		f[i][temp]=true;
+	}
+	for (int i=1;i<=n;i++)
+		for (int j=1;j<=1000;j++) {
+			if  (f[i-1][j] || (j-d[i]>0 && f[i-1][j-d[i]]))
+				f[i][j]=true;
+		}
+	for (int i=1;i<1001;i++)
+		if  (f[n][i]==false)
+			printf("%d ",i);
+	return 0;
+}
