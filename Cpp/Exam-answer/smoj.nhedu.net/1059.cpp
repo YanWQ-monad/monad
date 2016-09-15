ba5306ffc1346b65caedd3583cf821664efbb66e
YanWQ-monad <yanwqmonad@gmail.com>
2016-09-16 03:23:48 +0800

    Update 1059.cpp

--- Cpp/Exam-answer/smoj.nhedu.net/1059.cpp
+++ Cpp/Exam-answer/smoj.nhedu.net/1059.cpp
@@ -6,7 +6,7 @@ int main() {
 	freopen("1059.in","r",stdin);
 	freopen("1059.out","w",stdout);
 	scanf("%d",&n);
-	for	(int i=0;i<n;i++)
+	for (int i=0;i<n;i++)
 		ans=(ans+1)*2;
 	printf("%d",ans);
 	return 0;
