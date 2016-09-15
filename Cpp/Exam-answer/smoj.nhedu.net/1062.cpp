c0f533a566b653eaf363ba7f2c4d61f428d2a412
YanWQ-monad <yanwqmonad@gmail.com>
2016-09-16 03:28:10 +0800

    Create 1062.cpp

---
+++ Cpp/Exam-answer/smoj.nhedu.net/1062.cpp
@@ -0,0 +1,11 @@
+// 1062.cpp
+#include <cstdio>
+
+int a,b;
+int main() {
+	freopen("1062.in","r",stdin);
+	freopen("1062.out","w",stdout);
+	scanf("%d %d",&a,&b);
+	printf("%lld",(long long)a*b);
+	return 0;
+}
