2da3263bcfea23f27c6dbb0fdfc8679c0b0439de
YanWQ-monad <yanwqmonad@gmail.com>
2016-09-16 03:53:19 +0800

    Create 1075.cpp

---
+++ Cpp/Exam-answer/smoj.nhedu.net/1075.cpp
@@ -0,0 +1,12 @@
+// 1075.cpp
+#include <cstdio>
+int n;
+int main() {
+	freopen("1075.in","r",stdin);
+	freopen("1075.out","w",stdout);
+	scanf("%d",&n);
+	for (int i=0;i<n;i++)
+		printf("%d ",i);
+	printf("\n");
+	return 0;
+}
