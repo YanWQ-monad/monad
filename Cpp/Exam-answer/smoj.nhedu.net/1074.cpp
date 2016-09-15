8ab8d2a4e60cb81bc3cc46cfc5c6303734e50b69
YanWQ-monad <yanwqmonad@gmail.com>
2016-09-16 03:52:50 +0800

    Create 1074.cpp

---
+++ Cpp/Exam-answer/smoj.nhedu.net/1074.cpp
@@ -0,0 +1,10 @@
+// 1074.cpp
+#include <cstdio>
+int main() {
+	freopen("1074.in","r",stdin);
+	freopen("1074.out","w",stdout);
+	for (int i=10;i<100;i+=2)
+		printf("%d ",i);
+	printf("\n");
+	return 0;
+}
