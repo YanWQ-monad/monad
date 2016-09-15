e6fe145b9384c82b97499560374cadd9600688a0
YanWQ-monad <yanwqmonad@gmail.com>
2016-09-16 03:09:31 +0800

    Update 1040.cpp

--- Cpp/Exam-answer/smoj.nhedu.net/1040.cpp
+++ Cpp/Exam-answer/smoj.nhedu.net/1040.cpp
@@ -2,16 +2,14 @@
 #include <cstdio>
 
 #define mod 10007
-int n,m,c[5010][5010];
-int yang(int n,int m) {
-	if      (m==0 || m==n)                   return c[n][m]=1;
-	else if (n<5000 && m<5000 && c[n][m]!=0) return c[n][m];
-	else                                     return c[n][m]=(yang(n-1,m)+yang(n-1,m-1))%mod;
-}
+int n,m,c[2010][1010];
 int main() {
 	freopen("1040.in","r",stdin);
 	freopen("1040.out","w",stdout);
 	scanf("%d %d",&n,&m);
-	printf("%d\n",yang(n+m,m));
+	for (int i=0;i<=n+m;i++)
+		for (int j=0;j<=i;j++)
+			c[i][j]=(j==0||j==i)? 1:(c[i-1][j-1]+c[i-1][j])%mod;
+	printf("%d\n",c[n+m][m]);
 	return 0;
 }
