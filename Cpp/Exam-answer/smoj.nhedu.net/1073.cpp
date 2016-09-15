45ec280519f7a2be1a4f872c38c43f8a234acc2c
YanWQ-monad <yanwqmonad@gmail.com>
2016-09-16 03:52:14 +0800

    Create 1073.cpp

---
+++ Cpp/Exam-answer/smoj.nhedu.net/1073.cpp
@@ -0,0 +1,12 @@
+// 1073.cpp
+#include <cstdio>
+int n,m;
+int main() {
+	freopen("1073.in","r",stdin);
+	freopen("1073.out","w",stdout);
+	scanf("%d %d",&n,&m);
+	for (int i=n;i<=m;i++)
+		printf("%d ",i);
+	printf("\n");
+	return 0;
+}
