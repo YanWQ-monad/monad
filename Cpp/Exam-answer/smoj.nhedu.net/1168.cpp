37be0c6f56208fb1d582b99b071ff72951b30eb0
YanWQ-monad <yanwqmonad@gmail.com>
2016-09-16 04:13:27 +0800

    Update 1168.cpp

--- Cpp/Exam-answer/smoj.nhedu.net/1168.cpp
+++ Cpp/Exam-answer/smoj.nhedu.net/1168.cpp
@@ -1,19 +1,13 @@
 // 1168.cpp
 #include <cstdio>
 int n;
-long long ans=1,f1=1,f2=1;
-long long pow_ten(int n) {
-	long long ans=1;
-	for	(int i=0;i<n;i++)
-		ans*=10;
-	return ans;
-}
+long long ans=1,now=1;
 int main() {
 	freopen("1168.in","r",stdin);
 	freopen("1168.out","w",stdout);
 	scanf("%d",&n);
-	for	(int i=0;i<n-1;i++)
-		ans=ans*10+pow_ten(i)*9;
+	for	(int i=0;i<n-1;i++,now*=10)
+		ans=ans*10+now*9;
 	printf("%lld",ans);
 	return 0;
 }
