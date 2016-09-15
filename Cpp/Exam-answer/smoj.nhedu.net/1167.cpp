e4e1edc7054113404d0e7c9a6b3593f6711f6f6a
YanWQ-monad <yanwqmonad@gmail.com>
2016-09-16 04:14:20 +0800

    Update 1167.cpp

--- Cpp/Exam-answer/smoj.nhedu.net/1167.cpp
+++ Cpp/Exam-answer/smoj.nhedu.net/1167.cpp
@@ -1,34 +1,29 @@
 // 1167.cpp
 #include <cstdio>
-int n,c,w[50001];
-bool check(int m) {
-	if  (m>n)   return false;
-	int eat=0,full=0;
-	for (int i=0;i<n;i++) {
-		eat++;
-		full+=w[i];
-		if	(eat>=m) {
-			full-=w[i-m];
-			if	(full<=c)    return true;
-		}
-	}
-	return false;
-}
-int find() {
-	int l=0,r=n+1,mid;
+int n,c,w[50001],sum[50001],max;
+int find(int m) {
+	int l=0,r=n,mid;
 	while   (l+1<r) {
 		mid=(l+r)/2;
-		if  (check(mid))    l=mid;
+		if  (sum[mid]-sum[m-1]<=c)	l=mid;
 		else    r=mid;
 	}
-	return l;
+	return l-m+1;
 }
 int main() {
 	freopen("1167.in","r",stdin);
 	freopen("1167.out","w",stdout);
 	scanf("%d %d",&n,&c);
-	for (int i=0;i<n;i++)
+	scanf("%d",&w[0]);
+	sum[0]=w[0];
+	for (int i=1;i<n;i++) {
 		scanf("%d",&w[i]);
-	printf("%d",find());
+		sum[i]=sum[i-1]+w[i];
+	}
+	for (int i=0,temp;i<n;i++) {
+		temp=find(i);
+		max=temp>max? temp:max;
+	}
+	printf("%d\n",max);
 	return 0;
 }
